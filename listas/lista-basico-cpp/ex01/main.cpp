c499a8afbbe38784f1303b81d3b3135c343a7342
c0utin <rafaelcouto111@gmail.com>
2026-02-11 20:17:44 -0300

    ex01

--- listas/lista-basico-cpp/ex01/main.cpp
+++ listas/lista-basico-cpp/ex01/main.cpp
@@ -18,9 +18,9 @@ using namespace std;
 int main() {
     string nome;
     
-    // TODO: Leia o nome do usuário
-    
-    // TODO: Exiba a saudação personalizada
+    getline(cin, nome);
+
+    cout << "Olá, " << nome << "!" << endl;
     
     return 0;
 }
